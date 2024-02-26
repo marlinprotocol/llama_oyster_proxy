ea7499649a985489706f814aecbca0e1cfbbede2
sagarparker <sagar8parker@gmail.com>
2024-02-26 15:57:44 +0530

    Added RLP with alloy for signature parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::{net::ToSocketAddrs, str::from_utf8};
 
+use actix_web::web::BytesMut;
 use actix_web::{
     dev::PeerAddr, error, http::header, middleware, web, App, Error, HttpRequest, HttpResponse,
     HttpServer,
 };
+use alloy_rlp::{Encodable, RlpDecodable, RlpEncodable};
 use anyhow::Context;
 use awc::Client;
 use clap::Parser;
 use k256::elliptic_curve::generic_array::sequence::Lengthen;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use std::time::Duration;
 use tiny_keccak::{Hasher, Keccak};
 use tokio::fs;
+use tokio_stream::StreamExt;
 use url::Url;
-use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
 pub struct OllamaResponse {
@@ -21,18 +23,35 @@ pub struct OllamaResponse {
     pub created_at: String,
     pub response: String,
     pub done: bool,
-    pub context: Vec<u128>,
+    pub context: Vec<u32>,
     pub total_duration: u128,
     pub load_duration: u128,
-    pub prompt_eval_duration:u128,
+    pub prompt_eval_duration: u128,
     pub eval_count: u128,
-    pub eval_duration: u128
+    pub eval_duration: u128,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OllamaRequest {
+    pub model: String,
+    pub prompt: String,
+    pub stream: Option<bool>,
+    pub context: Option<Vec<u128>>,
+}
+
+#[derive(Debug, RlpEncodable, RlpDecodable, PartialEq)]
+pub struct OllamaSignatureParameters {
+    pub model: String,
+    pub prompt: String,
+    pub request_context: String,
+    pub response_context: String,
+    pub response: String,
 }
 
 /// Forwards the incoming HTTP request using `awc`.
 async fn forward(
     req: HttpRequest,
-    payload: web::Payload,
+    mut payload: web::Payload,
     peer_addr: Option<PeerAddr>,
     url: web::Data<Url>,
     client: web::Data<Client>,
@@ -42,7 +61,8 @@ async fn forward(
     new_url.set_query(req.uri().query());
     let forwarded_req = client
         .request_from(new_url.as_str(), req.head())
-        .no_decompress().timeout(Duration::new(60, 0));
+        .no_decompress()
+        .timeout(Duration::new(60, 0));
 
     let forwarded_req = match peer_addr {
         Some(PeerAddr(addr)) => {
@@ -50,6 +70,24 @@ async fn forward(
         }
         None => forwarded_req,
     };
+
+    let mut ollama_request_bytes = BytesMut::new();
+    while let Some(item) = payload.next().await {
+        let item = item?;
+        ollama_request_bytes.extend_from_slice(&item);
+    }
+
+    let mut ollama_request_body: OllamaRequest =
+        serde_json::from_str(from_utf8(&ollama_request_bytes)?)?;
+
+    ollama_request_body.stream = Some(false);
+
+    let model_prompt = &ollama_request_body.prompt;
+    let model_request_context = match &ollama_request_body.context {
+        Some(data) => serde_json::to_string(data)?,
+        None => "[]".to_string(),
+    };
+
 
     // get the host header value
     let host_header = req
@@ -59,7 +97,7 @@ async fn forward(
         .and_then(|x| x.to_str().context("could not parse Host header"));
     let host_header = host_header.unwrap().to_owned();
     let mut res = forwarded_req
-        .send_stream(payload)
+        .send_json(&ollama_request_body)
         .await
         .map_err(error::ErrorInternalServerError)?;
 
@@ -95,16 +133,26 @@ async fn forward(
             .unwrap_or("")
             .as_bytes(),
     );
-    // let ollama_res = res.json::<OllamaResponse>().await.unwrap();
-    // log::info!("{}",ollama_res.response);
+
     let body = res.body().await?;
     let ollama_response: OllamaResponse = serde_json::from_str(from_utf8(&body)?)?;
-    // let model_name = ollama_response.model;
-    // let model_response = ollama_response.response;
-    // let model_context = ollama_response.context;
-    
-    hasher.update(b"|body|");
-    hasher.update(&body);
+    let model_name = ollama_response.model;
+    let model_response = ollama_response.response;
+    let model_response_context = serde_json::to_string(&ollama_response.context)?;
+
+    let ollama_signature_parameters = OllamaSignatureParameters {
+        model: model_name,
+        prompt: model_prompt.to_string(),
+        request_context: model_request_context,
+        response: model_response,
+        response_context: model_response_context,
+    };
+
+    let mut rlp_encoded_parameters = Vec::<u8>::new();
+    ollama_signature_parameters.encode(&mut rlp_encoded_parameters);
+
+    hasher.update(b"|ollama_signature_parameters|");
+    hasher.update(&rlp_encoded_parameters);
     hasher.update(b"|host|");
     hasher.update(host_header.as_bytes());
 
@@ -154,8 +202,6 @@ async fn main() -> std::io::Result<()> {
     );
 
     log::info!("forwarding to {forward_url}");
-
-
 
     HttpServer::new(move || {
         App::new()
